230658a89cbe33711a5dc05b38647f128bef1d68
Dang Quoc Huy <huysamdua@yahoo.com>
2016-08-31 23:22:22 +0700

    Bonus: Create a Custom Modal Transition

--- TinderFake/CardsViewController.swift
+++ TinderFake/CardsViewController.swift
@@ -12,6 +12,8 @@ class CardsViewController: UIViewController {
 
     @IBOutlet weak var imageView: DraggableImageView!
     
+    var isPresenting: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,7 +32,76 @@ class CardsViewController: UIViewController {
         // Register Nib
         let profileView = ProfileViewController(nibName: "ProfileViewController", bundle: nil)
         profileView.image = imageView.image!
+        
+        profileView.modalPresentationStyle = UIModalPresentationStyle.Custom
+        profileView.transitioningDelegate = self
+        
         // Present View "Modally"
-        presentViewController(profileView, animated: false, completion: nil)
+        presentViewController(profileView, animated: true, completion: nil)
+    }
+}
+
+extension CardsViewController: UIViewControllerTransitioningDelegate {
+
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = true
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresenting = false
+        return self
+    }
+}
+
+extension CardsViewController: UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        
+        // The value here should be the duration of the animations scheduled in the animationTransition method
+        return 0.4
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        if (isPresenting) {
+            animatePresentationWithTransitionContext(transitionContext)
+        } else {
+            animateDismissalWithTransitionContext(transitionContext)
+        }
+    }
+    
+    private func animatePresentationWithTransitionContext(transitionContext: UIViewControllerContextTransitioning) {
+        
+        guard let containerView = transitionContext.containerView(),
+            let toViewControllerView = transitionContext.viewForKey(UITransitionContextToViewKey),
+            let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) else {
+                return
+        }
+        
+        // Position the presented view off the top of the container view
+        toViewControllerView.frame = transitionContext.finalFrameForViewController(toViewController)
+        
+        containerView.addSubview(toViewControllerView)
+        toViewController.view.alpha = 0
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            toViewController.view.alpha = 1
+        }) { (finished: Bool) -> Void in
+            transitionContext.completeTransition(true)
+        }
+    }
+    
+    private func animateDismissalWithTransitionContext(transitionContext: UIViewControllerContextTransitioning) {
+        
+        guard let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey) else {
+                return
+        }
+        
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            fromViewController.view.alpha = 0
+        }) { (finished: Bool) -> Void in
+            transitionContext.completeTransition(true)
+            fromViewController.view.removeFromSuperview()
+        }
     }
 }
